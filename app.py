154d58330a87b3c3a838dffb8470292265e0aa80
Fabians2898 <fabians2898@gmail.com>
2016-06-29 11:38:28 -0500

    correct error in import os, app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import tornado.ioloop
 import tornado.web
 import tornado.httpserver
 
-import os.path
-import os.environ
+import os
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
